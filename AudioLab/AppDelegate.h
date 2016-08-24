d3801c541e2032ce7fabd3b98fddc5d8167363f7
Eric Larson <eclarson@erics-macbook-pro.local>
2016-08-24 15:17:16 -0500

    Initial Commit

---
+++ AudioLab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioLab
+//
+//  Created by Eric Larson on 8/24/16.
+//  Copyright © 2016 Eric Larson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioLab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioLab
+//
+//  Created by Eric Larson on 8/24/16.
+//  Copyright © 2016 Eric Larson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
